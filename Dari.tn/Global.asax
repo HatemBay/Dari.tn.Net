9e9e4b2bd34578d8296b0cd8d64b569f78ba290c
hatem <hatem.bayoudh1@esprit.tn>
2021-04-08 14:47:44 +0100

    First

---
+++ Dari.tn/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dari.tn.MvcApplication" Language="C#" %>
---
+++ Dari.tn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dari.tn.Startup))]
+namespace Dari.tn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
